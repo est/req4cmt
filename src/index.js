6e262857349c796fafa124084ab3f94516c96db8
ᴇsᴛ <electronixtar@gmail.com>
2025-08-22 17:59:37 +0800

    get repo info .

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import http from "isomorphic-git/http/web";
 
 export default {
   async fetch(request, env, ctx) {
+    const r = await git.getRemoteInfo({ http, url: env.REPO })
+    console.info(r)
+
     return new Response('Hello World!');
   }
 };
